e6ce84015b8e9b1b2de34d58bdfd642712dd8318
Kevin Bailey <kbailey@freewayprojects.com>
2011-11-01 18:48:00 +0000

    Module is at the stage where vocabulary terms can be exported to a dataset file and the module can import terms from a dataset file.

--- README.txt
+++ README.txt
@@ -1,1 +1,63 @@
-Initial commit
+
+CONTENTS OF THIS FILE
+---------------------
+
+ * Overview
+
+OVERVIEW
+--------
+
+The purpose of this module is to provide a way to move data between
+Drupal instances. 
+
+This is not easy to achieve as Drupal stores configuration and data in
+the same database. Also, Drupal can be extended and customised which
+makes creating a standard data export/import module effectively
+impossible. 
+
+This module will provide a framework for exporting datasets as files
+and also for importing these dataset files. That way if the module is
+installed on two separate Drupal instances it will be possible to
+export data as a dataset file from one instance and import it into a
+different Drupal instance. 
+
+This module will be customisable and extendable via plugins or a
+similar method to take into account different Drupal builds. This
+means that if a Drupal instance has a very specialised setup it will
+be possible to create a new dataset profile to export/import data. 
+
+The module should include some initial dataset profiles which can be
+used on standard Drupal instances. These would enable the following
+datasets to be exported/imported. 
+
+ * Taxonomy vocabularies and terms
+ * Basic nodes such as page, story.
+ * Users
+
+When exporting all data should be exported - when importing the data
+should be used in a way to make sure that the receiving Drupal
+instance ends up with matching data. Careful checks will be needed to
+ensure ID's match, that the data remains consistent.
+
+Effectively this module will be similar to the backup_migrate module
+except that the backup_migrate profiles will be extended. Currently
+the backup_migrate profiles only allow for the choice of which tables
+will be exported/imported and a few other settings. This module will
+use the Druapl API to read data and produce rich data files which will
+contain all data needed to reproduce a particular dataset. It will
+probably also use the Drupal API to recreate data. It may also be
+possible to use SQL directly to export/import data.
+
+The primary usage for this module is envisioned to be exporting
+datasets from a live Drupal site and importing this live data into a
+new updated version of the site which developers have been working
+on. Once the import has completed then the newly developed site can
+become the new live site. This has the advantage of being able to fall
+back to the original live site if there is an unforseen problem
+between the new site and the live site data.
+
+It will be necessary to carry out proper checks when importing
+datasets. E.g.. when node data is being imported it will be necessary
+to check that the receiving site has the correct content type and
+fields etc. It may be necessary to use the UUID module to provide
+unique ID's.
--- test.php
+++ test.php
@@ -1,97 +1,22 @@
 <?php
 
-  // This file is used to run various functions etc from the command line.
-// Example command line:                                                                                                                            
-// drush @kbailey2_radiov2 php-script /home/kbailey2/projects/radiov2/web/trunk/sites/all/modules/data_export_import/test.php
+/**
+ * @file
+ * This file is used to run various functions etc from the command line.
+ *
+ * As we are developing batch operations it is useful to be able to
+ * test functions from the command line.  For example, this file can
+ * be called via drush using:
+ * drush @example_site php-script /home/username/projects/example_project/web/trunk/sites/all/modules/data_export_import/test.php
+ */
 
 require_once 'includes/taxonomy_terms.inc';
 
+// Test the export.
+//$dataset_file = data_export_import_export_taxonomy_terms();
+//echo "dataset file: ".$dataset_file."\n";
 
-// HERE - NEED TO SORT OUT WHAT HAPPENS IF A TERM HAS TWO PARENTS - THIS PRODUCES TWO TERM RECORDS 
-// WITH THE SAME tid 
-
-// THE FOLLOWING EXPORT/IMPORT TRIES TO UPDATE BOTH OF THESE RECORDS.
-
-// NEED TO IDENTIFY RECORDS BY vid,tid AND PARENTS.
-
-//data_export_import_export_taxonomy_terms();
-
-//data_export_import_import_taxonomy_terms("test_vocabularies");
-data_export_import_import_taxonomy_terms("20111026_174720_taxonomy_terms.dataset");
-
-//reset_taxonomy_vocabulary_hierarchy(10);
+// Test the import.
+data_export_import_import_taxonomy_terms("20111027_182140_taxonomy_terms.dataset");
 
 return;
-
-
-/*
-  $vocabularies_array2 = taxonomy_get_vocabularies();
-
-
-  // NEEDS TO BE MADE TO WORK!
-  foreach ($vocabularies_array2 as $vocabulary2) {
-
-
-    echo "NEEDS TO WORK";
-
-    $vocabulary_id2 = $vocabulary2->vid;
-    reset_taxonomy_vocabulary_hierarchy($vocabulary_id2);
-
-
-  }
-
-*/
-
-///session_destroy();
-
-//reset_taxonomy_vocabulary_hierarchy('18');
-
-
-return;
-
-
-
-$tree = taxonomy_get_tree_with_reset(18, 0, -1, NULL, TRUE);
-
-echo "here is the FIRST tree returned:\n";
-var_export($tree);
-
-
-taxonomy_del_term('1971');
-
-
-$tree = taxonomy_get_tree_with_reset(18, 0, -1, NULL, TRUE);
-
-echo "here is the SECOND tree returned:\n";
-var_export($tree);
-
-
-
-
-return;
-
-
-
-$loaded_term = taxonomy_get_term('1958');
-
-var_export($loaded_term);
-
-
-$term = array(
-	      'tid' => 1958, // If we add key 'tid' to this array then the function will update this term.
-	      'vid' => 18, // Voacabulary ID
-	      'name' => 'Drupalqwerty', // Term Name
-	      'description' => 'descriptin goes here initi',
-	      'synonyms' => "Drupletqweqwe\nsecond synonym init", // (Optional) Synonym of this term
-	      'weight' => 0,
-	      'parent' => array(0), // (Optional) Term ID of a parent term 
-	      'relations' => array(0), // (Optional) Related Term IDs
-	      );
-
-
-taxonomy_save_term($term);
-
-
-$loaded_term_after = taxonomy_get_term('1958');
-
-var_export($loaded_term_after);
