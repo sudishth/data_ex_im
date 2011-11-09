90a1e913e1fd8f200ee7a778e4a733c175dde5e8
Kevin Bailey <kbailey@freewayprojects.com>
2011-11-09 16:00:23 +0000

    Added in stubs for more profiles and tidied up code using coder_tough_love.

--- test.php
+++ test.php
@@ -7,17 +7,17 @@
  * As we are developing batch operations it is useful to be able to
  * test functions from the command line.  For example, this file can
  * be called via drush using:
- * drush @example_site php-script /home/username/projects/example_project/web/trunk/sites/all/modules/data_export_import/test.php
+ * drush @example_site php-script /path/to/test.php
  */
 
-require_once 'includes/taxonomy_terms.inc';
+require_once 'includes/profiles/taxonomy_terms.inc';
 require_once 'includes/profiles/articles.inc';
 
 // Test the export.
-$dataset_file = data_export_import_export_articles();
-echo "dataset file: " . $dataset_file . "\n";
+// $dataset_file = data_export_import_export_articles();
+// echo "dataset file: " . $dataset_file . "\n";
 
 // Test the import.
-// data_export_import_import_taxonomy_terms("20111103_164204_taxonomy_terms.dataset");
+data_export_import_import_taxonomy_terms("20111109_141030_taxonomy_terms.dataset");
 
 return;
